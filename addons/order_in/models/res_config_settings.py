013bf26a51370bb0983814d7c7e47a8bef02a632
Anucha.tk <anuchataowkaen@gmail.com>
2022-11-08 23:58:00 +0700

    :heavy_plus_sign: use setting to model

--- addons/order_in/models/res_config_settings.py
+++ addons/order_in/models/res_config_settings.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
+    """config order_in module"""
+
     _inherit = "res.config.settings"
 
-    cancel_days = fields.Integer(string="Cancel Days", config_parameter="order_in.cancel_days")
+    cancel_days = fields.Integer(
+        string="Cancel Days", config_parameter="order_in.cancel_days"
+    )
--- addons/order_in/wizard/cancel_appointment.py
+++ addons/order_in/wizard/cancel_appointment.py
@@ -1,25 +1,40 @@
-import datetime
+from datetime import date
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
+from dateutil import relativedelta
 
 
 class CancelAppointment(models.TransientModel):
+    """cancel appointment"""
+
     _name = "cancel.appointment.wizard"
     _description = "Cancel Appointment Wizard"
 
-    appointment_id = fields.Many2one("order.appointment", string="Appointment", domain=[("state", "=", "draft")])
+    appointment_id = fields.Many2one(
+        "order.appointment",
+        string="Appointment",
+        domain=[("state", "=", "draft")],
+    )
     reason = fields.Text(string="Reason")
     date_cancel = fields.Date(string="Cancellation Date")
 
+    # todo tree view not update when cancel day from heder dropdown
     def action_cancel(self):
-        if self.appointment_id.booking_date == fields.Date.today():
-            raise ValidationError(_("cancellation isn't allow in the same day of booking"))
+        """can cancel appointment before cancel_day param"""
+
+        cancel_day = self.env["ir.config_parameter"].get_param("order_in.cancel_days")
+        allow_date = self.appointment_id.booking_date + relativedelta.relativedelta(
+            days=int(cancel_day)
+        )
+        if allow_date < self.date_cancel:
+            raise ValidationError(_(f"can't cancel after {allow_date}"))
         self.appointment_id.state = "cancel"
-        return
 
     @api.model
-    def default_get(self, fields):
-        res = super(CancelAppointment, self).default_get(fields)
-        res["date_cancel"] = datetime.date.today()
+    def default_get(self, field):
+        """cancel appointment on self and not select appointment_id"""
+
+        res = super().default_get(field)
+        res["date_cancel"] = date.today()
         res["appointment_id"] = self.env.context.get("active_id")
         return res
