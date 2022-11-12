89095a545dcbb5b3fa2ec456b25bef9deca0a065
Anucha.tk <anuchataowkaen@gmail.com>
2022-11-12 15:09:15 +0700

    feat: add Monetary appointment pharmacy line

--- addons/order_in/models/appointment.py
+++ addons/order_in/models/appointment.py
@@ -3,6 +3,8 @@ from odoo.exceptions import ValidationError
 
 
 class OrderAppointment(models.Model):
+    """Order Appointment"""
+
     _name = "order.appointment"
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Order Appointment"
@@ -38,36 +40,57 @@ class OrderAppointment(models.Model):
         "appointment.pharmacy.lines", "appointment_id", string="Pharmacy Lines"
     )
     doctor_id = fields.Many2one("operation", string="Doctor")
+    company_id = fields.Many2one(
+        "res.company",
+        string="Company",
+        default=lambda self: self.env.company,
+    )
+    currency_id = fields.Many2one("res.currency", related="company_id.currency_id")
 
     @api.onchange("customer_id")
     def _onchange_customer_id(self):
         self.ref = self.customer_id.ref
 
     def action_in_consultation(self):
+        """action consultation"""
         for rec in self:
             if rec.state == "draft":
                 rec.state = "in_consultation"
 
     def action_done(self):
+        """action done"""
         for rec in self:
             rec.state = "done"
 
     def action_cancel(self):
+        """action cancel"""
         action = self.env.ref("order_in.action_cancel_appointment").read()[0]
         return action
 
     def unlink(self):
+        """prevent delete is not draft"""
         for rec in self:
             if rec.state != "draft":
                 raise ValidationError("You can delete appointment only draft status !")
-        return super(OrderAppointment, self).unlink()
+        return super().unlink()
 
 
 class AppointmentPharmacyLines(models.Model):
+    """Appointment Pharmacy"""
+
     _name = "appointment.pharmacy.lines"
     _description = "Appointment Pharmacy Lines"
 
     product_id = fields.Many2one("product.product", string="product", required=True)
     quantity = fields.Float(string="Quantity", required=True, default=1)
-    price_unit = fields.Float(related="product_id.list_price", required=True)
+    price_unit = fields.Float(related="product_id.list_price")
     appointment_id = fields.Many2one("order.appointment", string="Appointment")
+    currency_id = fields.Many2one("res.currency", related="appointment_id.currency_id")
+    price_subtotal = fields.Monetary(
+        string="Subtotal", compute="_compute_price_subtotal"
+    )
+
+    @api.depends("price_unit", "quantity")
+    def _compute_price_subtotal(self):
+        for rec in self:
+            rec.price_subtotal = rec.price_unit * rec.quantity
